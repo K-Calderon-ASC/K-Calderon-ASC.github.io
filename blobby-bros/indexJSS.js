50b2652dbeacd3a21da85fa3647e3d5f0e7ee014
K-Calderon-ASC <calderonkevin108@gmail.com>
2016-08-03 11:00:22 -0400

    d

--- blobby-bros/indexJSS.js
+++ blobby-bros/indexJSS.js
@@ -7,6 +7,7 @@ function setup() {
 	blobbyBoyImg = loadImage("blobby-boy.png");
 	blobbyBoy = createSprite(blobbyBoyX, blobbyBoyY);
 	blobbyBoy.addImage(blobbyBoyImg);
+	blobbyBoy.scale(.25);
 	background(0);
 	fill(0,255,0);
 	rect(0,500,600,100);
