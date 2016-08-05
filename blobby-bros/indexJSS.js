55f197d3751eb920bf11c5a9132e6d69b41cf628
K-Calderon-ASC <calderonkevin108@gmail.com>
2016-08-05 12:37:27 -0400

    Update indexJSS.js

--- blobby-bros/indexJSS.js
+++ blobby-bros/indexJSS.js
@@ -2,19 +2,19 @@ var gameOver = false;
 var blobbyBoy;
 var blobbyBoyY = 500;
 function setup() {
+	camera.on();
 	createCanvas(600,600);
 	blobbyBoyImg = loadImage("blobby-boy.png");
-	blobbyBoy = createSprite(width/2, blobbyBoyY);
+	blobbyBoy = createSprite(camera.position.x, blobbyBoyY);
 	blobbyBoy.addImage(blobbyBoyImg);
 	blobbyBoy.scale = .1;
 	background(0);
 	fill(0,255,0);
 	rect(0,500,600,100);
-	camera.on();
 }
 
 var blobbyBoyMovements = function() {
-	camera.position.x +=1 ;
+	camera.position.x +=5 ;
 	if(keyDown("x")){
 		console.log("are you running or nah");
 		blobbyBoy.position.y -= 10;
