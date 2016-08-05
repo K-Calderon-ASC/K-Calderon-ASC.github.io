665ade4b4984bdad8c69edf9d11917d384e784e3
K-Calderon-ASC <calderonkevin108@gmail.com>
2016-08-05 13:06:36 -0400

    Update indexJSS.js

--- blobby-bros/indexJSS.js
+++ blobby-bros/indexJSS.js
@@ -1,12 +1,20 @@
 var gameOver = false;
 var blobbyBoy;
 var blobbyBoyY = 500;
+var block;
+var blockX = 600;
+var blockY = 300;
+var x = 0;
 function setup() {
 	createCanvas(600,600);
 	blobbyBoyImg = loadImage("blobby-boy.png");
 	blobbyBoy = createSprite(width/2, blobbyBoyY);
 	blobbyBoy.addImage(blobbyBoyImg);
 	blobbyBoy.scale = .1;
+	blockImg = loadImage("block.png");
+	block = createSprite(blockX, blockY);
+	block.addImage(blockImg);
+	block.scale = .1;
 	background(0);
 	fill(0,255,0);
 	rect(0,500,600,100);
@@ -23,7 +31,15 @@ function draw() {
 	fill(0,255,0);
 	rect(0,500,600,100);
 	drawSprites();
+	x += 1
 	if(gameOver === false) {
+		if(x>=30) {
+			blockX -= 10;
+			if(blockX===0){
+				blockX = 600;
+				x = 0;
+			}
+		}
 		blobbyBoyMovements();
 	}
 }
