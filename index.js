b5703a6b1aa9048851c82bee208b1fbaf21a354e
K-Calderon-ASC <calderonkevin108@gmail.com>
2016-08-18 21:24:45 -0400

    a

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	$("#welcome").hide(0)
 	$(".picsforhome").hide(0)
+	$("#link").hide(0)
 	$("#welcome")
 		.delay(500)
 		.fadeIn(2000)
@@ -11,5 +12,49 @@ $(document).ready(function(){
 });
 
 $("#projects").click(function(){
-	$("#projects").animate({top:"40%"},1000)
+	$("#link").hide(0)
+	$(".picsforhome").animate({top:"60%"},1000)
+	$("#projects").animate({top:"25%"},1000)
+	$("#link")
+		.delay(1500)
+		.attr("href","sub-pages/projects.html")
+		.html("PROJECTS")
+		.css("left", "30%")
+		.fadeIn(1000);
+})
+
+$("#aboutme").click(function(){
+	$("#link").hide(0)
+	$(".picsforhome").animate({top:"60%"},1000)
+	$("#aboutme").animate({top:"25%"},1000)
+	$("#link")
+		.delay(1500)
+		.attr("href","sub-pages/aboutme.html")
+		.html("ABOUT ME")
+		.css("left", "50%")
+		.fadeIn(1000);
+})
+
+$("#otherstuff").click(function(){
+	$("#link").hide(0)
+	$(".picsforhome").animate({top:"60%"},1000)
+	$("#otherstuff").animate({top:"25%"},1000)
+	$("#link")
+		.delay(1500)
+		.attr("href","sub-pages/otherstuff.html")
+		.html("OTHER STUFF")
+		.css("left", "5%")
+		.fadeIn(1000);
+})
+
+$("#sayhi").click(function(){
+	$("#link").hide(0)
+	$(".picsforhome").animate({top:"60%"},1000)
+	$("#sayhi").animate({top:"25%"},1000)
+	$("#link")
+		.delay(1500)
+		.attr("href","sub-pages/sayhi.html")
+		.html("SAY HI")
+		.css("left", "40%")
+		.fadeIn(1000);
 })
